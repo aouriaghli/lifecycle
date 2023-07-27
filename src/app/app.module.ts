5c89c95670eee5822e4dce5e2730a6fba60b184e
Ayoub Ouriaghli Romeo <ayoub.ouriaghli@soprasteria.com>
2023-07-27 13:47:09 +0200

    Fin Sec20. LifeCycle Hooks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './products/pages/product/product.component';
+import { ProductsModule } from './products/products.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    ProductsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
